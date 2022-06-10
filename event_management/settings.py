eb714c488ad6f68324cfcf659a4aa91f9e508222
heli <helis.inexture@gmail.com>
2022-06-10 11:21:14 +0530

    Add allowed hosts in settings.py

--- event_management/settings.py
+++ event_management/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-pc=a_s4b#w5sfmgl%!!=%9ziba!8wk0--qb0v06as)pwynz&za
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'heli-event-management.herokuapp.com']
 
 
 # Application definition
