635f4414b38d4d6c251c24c338a43311fae074d4
heli <helis.inexture@gmail.com>
2022-06-15 13:10:15 +0530

    update testimonials url

--- events/urls.py
+++ events/urls.py
@@ -8,7 +8,7 @@ router = DefaultRouter()
 router.register('testimonial', TestimonialsViewSet, basename='testimonial')
 
 urlpatterns = [
-    path('testimonials/', ViewTestimonials.as_view(), name='testimonials'),
+    path('view_testimonials/', ViewTestimonials.as_view(), name='view_testimonials'),
     path('question_answer/', QuestionAnswersView.as_view(), name='question_answer'),
     path('add_question/', AddQuestionView.as_view(), name='add_question'),
     path('add_answer/<int:id>/', AddAnswerView.as_view(), name='add_answer'),
