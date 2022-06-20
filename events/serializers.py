155ecbe9430b832aa7a66f501c70b955c6fd98ab
heli <helis.inexture@gmail.com>
2022-06-20 18:44:54 +0530

    event review update: remove display username instead of user id

--- events/serializers.py
+++ events/serializers.py
@@ -174,11 +174,6 @@ class EventReviewSerializer(serializers.ModelSerializer):
         model = EventReviews
         fields = ['user', 'event_idea_id', 'event_review']
 
-    user = serializers.SerializerMethodField()
-
-    def get_user(self, obj):
-        return obj.user.username
-
 
 class GetEventIdeasSerializer(serializers.ModelSerializer):
     """
