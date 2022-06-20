115fc6c27bd576233e531456c8546b3f19652db2
heli <helis.inexture@gmail.com>
2022-06-20 15:50:02 +0530

    set debug false

--- event_management/settings.py
+++ event_management/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-pc=a_s4b#w5sfmgl%!!=%9ziba!8wk0--qb0v06as)pwynz&za
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG')
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'heli-event-management.herokuapp.com']
 
